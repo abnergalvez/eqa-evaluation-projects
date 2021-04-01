7ab4fc317c39eca086d785ede493501897f2f064
Abner Gálvez Contreras <agalvez@erpeqa.es>
2021-03-31 22:29:05 -0300

    feat(user): #10 creacion de CRUD de usuarios

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,9 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::resource(name:'users', controller:UserController::class);
+Route::group( ['middleware'=>'auth'] ,function () {
+
+    Route::resource('users',App\Http\Controllers\UserController::class);
+
+});
+
